e1ae07e0ef88cd50a063cc83ab713fad93f283fa
DavidWeisdorf <weisdorf.david@gmail.com>
2020-04-05 21:38:29 -0400

    rename updates

--- server/renames.py
+++ server/renames.py
@@ -2,12 +2,26 @@
 import os
 from os.path import join, isfile, isdir
 
-dirpaths = [p for p in os.listdir('cities') if '.' not in p]
-print(dirpaths)
+def sanitize(s):
+	return s.lower().replace(' ', '-')
 
-for dirpath in dirpaths:
-	paths = os.listdir(join('cities', dirpath))
+def rename_under_dir(base):
+	paths = [p for p in os.listdir(base)]
+
 	for p in paths:
-		oldpath = join('cities', dirpath, p)
-		newpath = oldpath.lower().replace('-', '_')
-		os.rename(oldpath, newpath)
+		path = join(base, p)
+		if isdir(path):
+			rename_under_dir(path)
+		os.rename(path, sanitize(path))
+
+rename_under_dir('cities')
+
+# dirpaths = [p for p in os.listdir('cities') if '.' not in p]
+# print(dirpaths)
+
+# for dirpath in dirpaths:
+# 	paths = os.listdir(join('cities', dirpath))
+# 	for p in paths:
+# 		oldpath = join('cities', dirpath, p)
+# 		newpath = oldpath.lower().replace('-', '_')
+# 		os.rename(oldpath, newpath)
